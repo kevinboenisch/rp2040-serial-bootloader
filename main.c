6672b29b7947dad49fb3610c896912c493c6bbb6
danilom <danilom@gmail.com>
2025-02-25 22:09:42 +0700

    Bootloader size increase to 52kb

--- main.c
+++ main.c
@@ -29,12 +29,11 @@
 #include "stdalign.h"
 
 // Bootloader size. Must be 4k aligned. 
-// Currently, ~47k fits, but using 48k for alignment.
 // Was 12k originally. Make sure to match:
 // 1) danilom_bootloader/bootloader.ld
 // 2) jpo-software/resources/build_config/jpo_bootloadable.ld
 // 3) danilom_micropython/ports/rp2/jpo_memmap_mp.ld
-#define BOOTLOADER_SIZE_KB 48
+#define BOOTLOADER_SIZE_KB 52
 
 // The bootloader can be entered in three ways:
 //  - BOOTLOADER_ENTRY_PIN is low
